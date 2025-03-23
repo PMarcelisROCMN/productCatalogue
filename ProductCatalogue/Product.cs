bc79b381e062dbfd7886e0062b788c5879bdb6f0
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2025-03-23 15:59:27 +0100

    initial project setup

---
+++ ProductCatalogue/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace ProductCatalogue
+{
+    public class Product
+    {
+
+        public Product(string name, decimal price, string shortDesc)
+        {
+            Name = name;
+            Price = price;
+            ShortDescription = shortDesc;
+        }
+
+        public string Name { get; private set; }
+        public decimal Price { get; private set; }
+        public string ShortDescription { get; private set; }
+    }
+}
