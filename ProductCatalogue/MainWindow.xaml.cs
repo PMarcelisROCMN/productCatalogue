465b071bb605fd1218331e769b1cf0527bb23f88
PMarcelisROCMN <162563162+PMarcelisROCMN@users.noreply.github.com>
2025-03-23 16:04:21 +0100

    removed functionality for lesson

--- ProductCatalogue/MainWindow.xaml.cs
+++ ProductCatalogue/MainWindow.xaml.cs
@@ -18,23 +18,6 @@ namespace ProductCatalogue
             // Create some sample products
             _products = new ObservableCollection<Product>
             {
-                new Product (
-                   "Laptop",
-                    999.99m,
-                    "Powerful and portable laptop for work and play."
-                    ),
-                new Product(
-                
-                    "Smartphone",
-                    499.99m,
-                   "High-performance smartphone with a stunning display."
-               ),
-                new Product
-                (
-                  "Headphones",
-                    199.99m,
-                    "Noise-cancelling over-ear headphones with deep bass."
-                )
             };
 
             // Set initial product context
@@ -43,30 +26,14 @@ namespace ProductCatalogue
 
         private void BtnPrev_Click(object sender, RoutedEventArgs e)
         {
-            // Move index one step back, if possible
-            if (_currentIndex > 0)
-            {
-                _currentIndex--;
                 this.DataContext = _products[_currentIndex];
-            } else
-            {
-                _currentIndex = _products.Count - 1;
-                this.DataContext = _products[_currentIndex];
-            }
+            
         }
 
         private void BtnNext_Click(object sender, RoutedEventArgs e)
         {
-            // Move index one step forward, if possible
-            if (_currentIndex < _products.Count - 1)
-            {
-                _currentIndex++;
                 this.DataContext = _products[_currentIndex];
-            } else
-            {
-                _currentIndex = 0;
-                this.DataContext = _products[_currentIndex];
-            }
+            
         }
     }
 }
